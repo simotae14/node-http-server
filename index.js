99b9039eecf6bd378a53e465b1771ec36ec7631c
Simone <startae14@gmail.com>
2022-02-18 12:24:37 +0100

    handle the 404 response

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ server.on('request', (req, res) => {
     res.write('</body>');
     res.write('</html>');
     res.end();
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
-
 });
 
 server.listen(PORT, () => {
